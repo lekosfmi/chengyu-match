e8da01aa05b5200574575364cf8a26737ecef976
lephankoby <lephankoby@gmail.com>
2016-10-21 17:23:36 -0500

    adding score counter

--- helper/match.py
+++ helper/match.py
@@ -11,11 +11,16 @@
 
 def match(vocab_to_match, vocab_by_user):
 
+    counter = 0
+
     for i in range(len(vocab_to_match)):
         print("### " + vocab_to_match[i] + " ###")
-        user_input = input("> ")
+        user_input = input("> ").replace(" ", "")
 
         if user_input == vocab_by_user[i]:
+            counter += 1
             print('>> 👍  <<<')
         else:
             print(">>> " + vocab_to_match[i] + " <<<")
+
+    print('Score: {0}/{1} 🚀'.format(counter, len(vocab_to_match)))
