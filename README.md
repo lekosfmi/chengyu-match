dab28e21b0d97264c3d8115818c3bde27cfffc1e
Vanielle Le <lephanvanielle@gmail.com>
2016-10-20 22:36:36 -0500

    adding git ignore and updating readme

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ A fun matching games with 成語 (Chinese idioms) and their English translation 
 
 ### Happy gaming! 🚀
 
+## A Few Notes
+- Program only accepts traditional Chinese characters (at the moment)
+- The list of idioms are bared so feel free to send me your favorite idioms, so I can add it to the list!
+
 ##### Feel free to fork or send in pull request to contribute to this project!
 
 ##### Have a question? Don't hesitate to contact me: `lephanvanielle@gmail.com`
--- helper/match.py
+++ helper/match.py
@@ -11,19 +11,15 @@ shuffle(eng_chengyu)
 
 def eng_match():
     for i in range(len(chin_chengyu)):
-
         print("### " + chin_chengyu[i] + " ###")
         user_input = input("> ")
 
-        if user_input == eng_chengyu[i]:
-            print('⭕️')
+        print('⭕️') if user_input == eng_chengyu[i] else print('❌')
 
 
 def chin_match():
     for i in range(len(eng_chengyu)):
-
         print("### " + eng_chengyu[i] + " ###")
         user_input = input("> ")
 
-        if user_input == chin_chengyu[i]:
-            print('⭕️')
+        print('⭕️') if user_input == chin_chengyu[i] else print('❌')
